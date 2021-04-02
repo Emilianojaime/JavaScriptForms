9a877c058a5e3ef848fa48fabf684ed3e63d823b
Emiliano Jaime <emilianojaime123@gmail.com>
2021-04-02 14:38:43 -0300

    some validations for the fields are done (password, email, name) there are still missing validations for labels and dom elements to be done

---
+++ js/register.js
@@ -0,0 +1,104 @@
+const expressions = {
+	name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+	password: /^.{4,12}$/, // 4 a 12 digitos.
+	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+}
+
+const fields = {
+	fullname: false,
+	email: false,
+	password: false,
+	repeatPassword: false
+}
+
+const form = document.getElementById('form');
+const inputs = document.querySelectorAll('#form input');
+
+const validationForm = (e) => {
+	switch (e.target.name) {
+		case 'fullname':
+			if(expressions.name.test(e.target.value)){
+				document.getElementById('group__fullname').classList.remove('form__group-error');
+				document.querySelector('form .form__message').classList.remove('form__message-active');
+				document.querySelector('form .form__message .error__name').classList.remove('error__name-active');
+				fields['fullname'] = true;
+			} else {
+					document.getElementById('group__fullname').classList.add('form__group-error');
+					document.querySelector('form .form__message').classList.add('form__message-active');
+					document.querySelector('form .form__message .error__name').classList.add('error__name-active');
+					fields['fullname'] = false;
+			}	
+		break;
+		case 'email':
+			if(expressions.email.test(e.target.value)){
+				document.getElementById('group__email').classList.remove('form__group-error');
+				document.querySelector('form .form__message').classList.remove('form__message-active');
+				document.querySelector('form .form__message .error__email').classList.remove('error__email-active');
+				fields['email'] = true;
+			} else {
+					document.getElementById('group__email').classList.add('form__group-error');
+					document.querySelector('form .form__message').classList.add('form__message-active');
+					document.querySelector('form .form__message .error__email').classList.add('error__email-active');
+					fields['email'] = false;
+			}	
+		break;
+		case 'password':
+			if(expressions.password.test(e.target.value)){
+				document.getElementById('group__password').classList.remove('form__group-error');
+				document.querySelector('form .form__message').classList.remove('form__message-active');
+				document.querySelector('form .form__message .error__password').classList.remove('error__password-active');
+				fields['password'] = true;
+				
+			} else {
+					document.getElementById('group__password').classList.add('form__group-error');
+					document.querySelector('form .form__message').classList.add('form__message-active');
+					document.querySelector('form .form__message .error__password').classList.add('error__password-active');
+					fields['password'] = false;
+			}	
+		break;
+		case 'repeat-password':
+			const inputpasssword1 = document.getElementById('password');
+			const inputpasssword2 = document.getElementById('repeat-password');
+
+			if(inputpasssword1.value == inputpasssword2.value){
+				document.getElementById('group__repeat-password').classList.remove('form__group-error');
+				document.querySelector('form .form__message').classList.remove('form__message-active');
+				document.querySelector('form .form__message .error__repeat-password').classList.remove('error__repeat-password-active');
+				fields['repeatPassword'] = true;
+				
+			} else {
+					document.getElementById('group__repeat-password').classList.add('form__group-error');
+					document.querySelector('form .form__message').classList.add('form__message-active');
+					document.querySelector('form .form__message .error__repeat-password').classList.add('error__repeat-password-active');
+					fields['repeatPassword'] = false;
+					
+			}	
+		break;
+	}
+
+}
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', validationForm)
+	input.addEventListener('blur', validationForm)
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+		if (fields.fullname && fields.email && fields.password && fields.repeatPassword) {
+			form.reset();
+
+			document.querySelector('form .form__message-sent').classList.add('form__message-sent-active');
+			setTimeout(() => {
+				document.querySelector('form .form__message-sent').classList.remove('form__message-sent-active');
+			}, 6000);
+		} else {
+			document.querySelector('form .form__message').classList.add('form__message-active');
+			document.querySelector('form .form__message .error__missing-inputs').classList.add('error__missing-inputs-active')
+			setTimeout(() => {
+				document.querySelector('form .form__message').classList.remove('form__message-active');
+				document.querySelector('form .form__message .error__missing-inputs').classList.remove('error__missing-inputs-active')
+			}, 6000);
+		}
+
+});
